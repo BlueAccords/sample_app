a2a66f04f7d7dc97e0c033e4d913438684825297
BlueAccords <myoldemailname@gmail.com>
2015-05-21 15:22:11 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -20,6 +20,9 @@ class StaticPagesControllerTest < ActionController::TestCase
     # assert_select checks for html tag, then content inside the tag?
   end
   
-  
-  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
 end
