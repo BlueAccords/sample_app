ddb5e80c128174db30ead1d16f39b77915d9aade
BlueAccords <myoldemailname@gmail.com>
2015-06-04 23:27:51 +0000

    Added tests for valid account sign up with activation/invalid signup

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,20 @@
 class AccountActivationsController < ApplicationController
+  
+  # Finds user by email and checks to see if
+  # 1. user is found, 2. user is NOT activated already, 3. user's activation token and email are correct in the db.
+  # if true to all activate account and update attributes and log user in.
+  # else redirect to front page and give error message.
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated,   true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,18 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #log the user in if email is found in the db as belonging to a user
       # AND(&&) if they can be authenticated with the correct password.
-      log_in user
-      # creates temporary or semi permanent cookie session depending on checkbox
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      # checks to see if the user's account is activated or not.
+      if user.activated?
+        log_in user
+              # creates temporary or semi permanent cookie session depending on checkbox
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       # create an error message
