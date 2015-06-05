8752e409ad4431f54bc5a653c762ac48e7d36be0
BlueAccords <myoldemailname@gmail.com>
2015-06-05 12:12:08 +0000

    Basic tests for micropost model

---
+++ test/models/micropost_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class MicropostTest < ActiveSupport::TestCase
+  # test micropost has valid user, and within valid
+  # content length
+  
+  def setup
+    @user = users(:michael)
+    # This code is not idiomatically correct
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+  
+  test "micropost should be valid" do
+    assert @micropost.valid?
+  end
+  
+  test "user id should be present, invalid micropost" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+  
+  test "micropost content should be present" do
+    @micropost.content = "   "
+    assert_not @micropost.valid?
+  end
+  
+  test "content should be at most 140 char" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
+end
