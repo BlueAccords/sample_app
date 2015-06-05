2e3906697831e51cdb4e7e8e2bbf381a14584050
BlueAccords <myoldemailname@gmail.com>
2015-06-05 13:44:28 +0000

    Code refactoring to reuse logged_in_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -79,13 +79,13 @@ class UsersController < ApplicationController
   #Before filters
   
   # confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
+  # def logged_in_user
+  #   unless logged_in?
+  #     store_location
+  #     flash[:danger] = "Please log in."
+  #     redirect_to login_url
+  #   end
+  # end
   
   # Confirms the correct user
   def correct_user
