21fb80a84ba5f209792451c1f9d123c0fddd8ac9
BlueAccords <myoldemailname@gmail.com>
2015-06-06 23:10:11 +0000

    Changed back, fixed syntax error

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_crendentials = {
+    config.fog_credentials = {
       #Config for amazon s3
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
