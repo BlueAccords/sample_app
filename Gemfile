6d5bd5707b7a5f0a3dff5dad025a8f957007f3b9
BlueAccords <myoldemailname@gmail.com>
2015-06-06 20:48:21 +0000

    Added image uploading, on final steps of ch11

--- Gemfile
+++ Gemfile
@@ -7,6 +7,10 @@ gem 'rails', '4.2.0'
 gem 'bcrypt', '3.1.7'
 # faker lets you make users for your site with no users
 gem 'faker',  '1.4.2'
+# image upload
+gem 'carrierwave', '0.10.0'
+gem 'mini_magick', '3.8.0'
+gem 'fog', '1.23.0'
 # paginates objects to be displayed as pages
 gem 'will_paginate', '3.0.7'
 # configures will_paginate to use bootstraps styles
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,19 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
+  # associates image with a model via CarrierWave
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140} 
+  # validate = custom validation
+  validate :picture_size
+  
+  private
+  
+    # validates size of uploaded picture
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "File size should be less than 5MB")
+      end
+    end
 end
